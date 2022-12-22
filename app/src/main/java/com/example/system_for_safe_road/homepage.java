5f738e70229c42afc99ed7b1bea98475af62f020
Abrar <abrarhasan1998@gmail.com>
2022-12-22 19:44:46 +0600

    added File homepage.java

---
+++ app/src/main/java/com/example/system_for_safe_road/homepage.java
@@ -0,0 +1,14 @@
+package com.example.system_for_safe_road;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
