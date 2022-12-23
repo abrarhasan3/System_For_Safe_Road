b42cafaec498c0e8f19ce44ddb295d10751737a3
Abrar <abrarhasan1998@gmail.com>
2022-12-24 00:22:45 +0600

    Route can be viewed From Admin Window

--- app/src/main/java/com/example/system_for_safe_road/All_Routes_Activity.java
+++ app/src/main/java/com/example/system_for_safe_road/All_Routes_Activity.java
@@ -2,8 +2,15 @@ package com.example.system_for_safe_road;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,6 +19,22 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.directions.route.AbstractRouting;
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
+import com.example.system_for_safe_road.databinding.ActivityUserRouteGpsactivityBinding;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,9 +42,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class All_Routes_Activity extends AppCompatActivity {
+public class All_Routes_Activity extends AppCompatActivity  {
 
    DatabaseReference reference_route;
    AutoCompleteTextView autoCompleteTextView;
@@ -29,6 +53,13 @@ public class All_Routes_Activity extends AppCompatActivity {
    ArrayAdapter<String> arrayAdapter;
    Button confirm_button;
    String pass_to_next;
+
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +69,12 @@ public class All_Routes_Activity extends AppCompatActivity {
         autoCompleteTextView=findViewById(R.id.auto_complete_txt_all_routes);
         confirm_button= findViewById(R.id.confirm_button_allroutes);
 
+
+
         preset();
+
+
+
     }
 
     public void preset()
@@ -79,4 +115,45 @@ public class All_Routes_Activity extends AppCompatActivity {
             }
         });
     }
+
+
+
+//
+//    @Override
+//    public void onRoutingFailure(RouteException e) {
+
+//    }
+//
+//    @Override
+//    public void onRoutingStart() {
+//
+//
+//    }
+//
+
+
+
+
+
+
+//
+//    @Override
+//    public void onRoutingCancelled() {
+//
+//    }
+//
+//    @Override
+//    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+//
+//    }
+
+//    @Override
+//    public void onMapReady(@NonNull GoogleMap googleMap) {
+//
+//    }
+//
+//    @Override
+//    public void onPointerCaptureChanged(boolean hasCapture) {
+//
+//    }
 }
